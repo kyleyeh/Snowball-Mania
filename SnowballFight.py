1400e73436828b25c4f244cceac1e01af2a55060
kyleyeh <kyle.yeh@d11.org>
2024-12-03 09:17:00 -0700

    Create SnowballFight.py

---
+++ SnowballFight.py
@@ -0,0 +1,19 @@
+''' 
+    Name: Snowball-Mania
+    Author: 
+    Date: 
+    Class: AP Computer Science Principles
+    Python: 
+'''
+
+def main():
+    # the main runner of the game
+	  # welcome the player, gather names, and run the snowball fight!
+    print("Welcome to Snowball Mania!")
+
+def hitResult(hitNum):
+    # based on the number that is passed in, return True or False 
+    # indicating if this was a hit or a miss
+    return false
+
+main()
